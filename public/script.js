0ce2f6405755b52116c1b73aa5d3940e0e1a9a95
ipopkov <igorpopkov86@gmail.com>
2022-09-17 18:24:01 +0300

    arrow

--- public/script.js
+++ public/script.js
@@ -82,3 +82,15 @@ document.getElementById('phone').addEventListener('input', function (e) {
     var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
     e.target.value = !x[2] ? x[1] : '(' + x[1] + ') '+ x[2] + (x[3] ? '-' + x[3] : '');
 });
+
+(function arrowMagic() {
+    arrow.onclick = function() {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
+    };
+    window.addEventListener('scroll', function() {
+        arrow.hidden = (scrollY < 100);
+    });
+})();
